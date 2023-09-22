ba7da3d05ddd5d1298fb662370193279e272e129
David Farfan <dfarfan@cantilever.com.mx>
2023-09-21 23:45:25 -0600

    feat: add 404

---
+++ layouts/404.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="contenedor404">
+    <img class="imgBig" src="/404.png">
+    <div class="contendorText text-center">
+      <h1 class="text-center">
+        404
+      </h1>
+      <h2>El Recurso solicitado no fue encontrado</h2>
+      <div class="contenedorBoton">
+        <v-btn
+          color="red"
+          @click="regresar"
+        >
+          Regresar
+        </v-btn>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RickMortyExplorerWebapp404',
+
+  data () {
+    return {
+
+    }
+  },
+
+  mounted () {
+
+  },
+
+  methods: {
+    regresar () {
+      this.$router.push('./')
+    }
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/404.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Error 404: Página no encontrada</h1>
+    <p>La página que estás buscando no existe.</p>
+    <!-- Puedes agregar enlaces o contenido adicional aquí. -->
+  </div>
+</template>
+
+<script>
+export default {
+  layout: '404'
+  // Opciones del componente, si es necesario
+}
+</script>
+
+  <style scoped>
+  /* Estilos CSS específicos para esta página 404 */
+  </style>
